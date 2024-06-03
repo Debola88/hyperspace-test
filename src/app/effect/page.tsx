1bf05720fe35a8c3d6a6577e2f4b423cbd878aed
Badejo Adebola <adebolabadejo11@gmail.com>
2024-06-03 08:59:30 +0100

    Minor fix- animation page

--- src/app/effect/page.tsx
+++ src/app/effect/page.tsx
@@ -15,7 +15,7 @@ const Animation: React.FC = () => {
 
 
   return (
-    <div className="flex flex-col overflow-hidden items-center min-h-screen justify-center bg-black">
+    <div className="flex px-5 flex-col overflow-hidden items-center min-h-screen justify-center bg-black">
       <h1 className="text-2xl font-bold mb-4 text-white">Animation Page</h1>
       <button
         onClick={navigateToHome}
@@ -23,11 +23,11 @@ const Animation: React.FC = () => {
       >
         Go Back to Home Page
       </button>
-      <div className='w-[500px] h-[400px]'>
+      <div className='max-w-[500px] h-[400px]'>
         <Lottie onComplete={() => {
         phoneRef.current?.setDirection(-1)
         phoneRef.current?.play()
-        }} lottieRef={phoneRef} loop={false} animationData={animationData}/>
+        }} lottieRef={phoneRef} loop={true} animationData={animationData}/>
       </div>
     </div>
   );
